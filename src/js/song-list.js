82ec5a6bba9e520b77501a67e22d798075719734
chengzi07 <yihanchen7@163.com>
2019-02-26 21:33:22 +0800

    点击歌曲时激活歌曲项

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -9,11 +9,17 @@
       let $el=$(this.el)
       $el.html(this.template)
       let {songs}=data
-      let liList=songs.map((song)=>$('<li></li>').text(song.name))
+      let liList=songs.map((song)=>
+        $('<li></li>').text(song.name).attr('data-song-id',song.id)
+      )
       $el.find('ul').empty()
       liList.map((domLi)=>{
         $el.find('ul').append(domLi)
       })
+    },
+    activeItem(li){
+      let $li=$(li)
+      $li.addClass('active').siblings('.active').removeClass('active')
     },
     clearActive(){
       $(this.el).find('.active').removeClass('active')
@@ -42,12 +48,16 @@
       this.getAllSongs()
     },
     getAllSongs(){
-      this.model.find().then(()=>{
+      return this.model.find().then(()=>{
         this.view.render(this.model.data)
       })
     },
     bindEvents(){
-    
+      $(this.view.el).on('click','li',(e)=>{
+        this.view.activeItem(e.currentTarget)
+	let songId=e.currentTarget.getAttribute('data-song-id')
+	window.eventHub.emit('select',{id:songId})
+      })
     },
     bindEventHub(){
       window.eventHub.on('upload',()=>{
